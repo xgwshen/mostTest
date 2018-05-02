c50602eabbd0002a0f151768e4e2bc0fc2714c68
Cyanphp <xiaguiwei@gmail.com>
2018-05-02 10:52:42 +0800

    checkcode.php xgw2

--- checkcode.php
+++ checkcode.php
@@ -4,10 +4,7 @@ defined('IN_PHPCMS') or exit('No permission resources.');
 $session_storage = 'session_'.pc_base::load_config('system','session_storage');
 pc_base::load_sys_class($session_storage);
 $checkcode = pc_base::load_sys_class('checkcode');
-if (isset($_GET['code_len']) && intval($_GET['code_len'])) $checkcode->code_len = intval($_GET['code_len']);
-if ($checkcode->code_len > 8 || $checkcode->code_len < 2) {
-	$checkcode->code_len = 4;
-}
+//xgw2
 if (isset($_GET['font_size']) && intval($_GET['font_size'])) $checkcode->font_size = intval($_GET['font_size']);
 if (isset($_GET['width']) && intval($_GET['width'])) $checkcode->width = intval($_GET['width']);
 if ($checkcode->width <= 0) {
