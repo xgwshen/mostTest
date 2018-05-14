3d489958f23289636dfdbabfe3407329ad75293c
xgwshen <xgwshen@gmail.com>
2018-05-14 11:13:39 +0800

    first

--- add_favorite.php
+++ add_favorite.php
@@ -43,5 +43,7 @@ $data = array('title'=>$title, 'url'=>$url, 'adddate'=>SYS_TIME, 'userid'=>$user
 /**
  * 分支 xgw1修改
  * 1.分支修改后，切换后你的修改将被重写，所以在切换分支前必须把修改提交(commit)或者保存（stash）起来
+ *
+ * 2.commit -m first
  */
 ?>
